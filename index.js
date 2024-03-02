08b494af6568a96523685888e8934cdb800208c3
arnab <arnabchatterjee.ac.2@gmail.com>
2024-03-02 23:57:07 +0530

    fix/database

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const app = express();
 const port = process.env.PORT || 3000;
 const fs = require("fs");
-const BitMoji = require("./schema")
+const BitMoji = require("./schema");
 const multer = require("multer");
 const path = require("path");
-const { getHobbies,generateBitmoji } = require("./services/Bitmoji");
+const { getHobbies, generateBitmoji } = require("./services/Bitmoji");
 const downloadImage = require("./services/downloadImage");
+const { default: mongoose } = require("mongoose");
 require("dotenv").config();
 const storage = multer.memoryStorage({
   // Destination to store image
@@ -26,17 +27,24 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 app.post("/avatar", upload.single("avatar"), async (req, res) => {
-  const id = req.body.id
-    const ans = await getHobbies(
-      req.file.buffer.toString("base64"),
-      req.file.mimetype
-    );
-    const bitmoji = await generateBitmoji(ans)
-    const uri = await downloadImage(bitmoji)
-    await new BitMoji({userId:id,bucketURI:bitmoji}).save()
-    res.json(uri)
+  const id = req.body.id;
+  const ans = await getHobbies(
+    req.file.buffer.toString("base64"),
+    req.file.mimetype
+  );
+  const bitmoji = await generateBitmoji(ans);
+  const uri = await downloadImage(bitmoji);
+  await new BitMoji({ userId: id, bucketURI: bitmoji }).save();
+  res.json(uri);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port http://127.0.0.1:${port}`);
-});
+mongoose
+  .connect(
+    "mongodb+srv://shivamshaw9005:project@cluster0.5pilrv9.mongodb.net/your-database-name"
+  )
+  .then(() => console.log("Db connected sccessfully"))
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port http://127.0.0.1:${port}`);
+    });
+  });
