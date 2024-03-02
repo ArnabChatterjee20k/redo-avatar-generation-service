36308c1d287f02c0e191e4b752c30758bdcb6e88
arnab <arnabchatterjee.ac.2@gmail.com>
2024-03-02 20:20:25 +0530

    feat/updated port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const fs = require("fs");
 const BitMoji = require("./schema")
 const multer = require("multer");
